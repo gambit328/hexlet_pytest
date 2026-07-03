ed93e84e4ab38f0350669feab0fb81d1d926b54b
gambit328 <73571911+gambit328@users.noreply.github.com>
2026-07-03 15:04:14 +0700

    delete prints from test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,9 +2,7 @@ from hexlet_pytest.main import reverse
 
 
 def test_reverse():
-    print("до assert")
     assert reverse("Hexlet") == "telxeH"
-    print("после assert")
 
 
 def test_reverse_for_empty_string():
